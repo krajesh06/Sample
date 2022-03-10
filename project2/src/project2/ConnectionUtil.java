879f07d3de18b30a41b1485de78a70cb7fbd3ca0
krajesh06 <krajeshsai06@gmail.com>
2022-03-10 16:07:27 +0530

    first commit

---
+++ project2/src/project2/ConnectionUtil.java
@@ -0,0 +1,32 @@
+package project2;
+
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.Statement;
+
+	public class ConnectionUtil {
+		
+		public static void main(String[] args) throws Exception {
+			
+			//Step 1 : Database Driver ( Optional)
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			//Step 2: Get the Connection
+			Connection connection = DriverManager.getConnection(
+					"jdbc:mysql://101.53.133.59:3306/revature_training_db","rev_user","rev_user");
+			System.out.println(connection);
+			
+			//Step 3: Execute a query
+			String query = "INSERT INTO rajesh (NAME) VALUES ('vino');";
+			
+			Statement statement = connection.createStatement();
+			int rows = statement.executeUpdate(query);
+			
+			System.out.println("No of rows inserted :" + rows);
+			
+			
+			
+		}
+
+	}
+
